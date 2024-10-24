948861e399b6b26f435cbc8951a7c36da1aa2d32
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-10-24 20:57:09 +0600

    Add tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-red-400">Hello React!</h1>
+    </div>
+  );
 }
 
 export default App;
