c98dae31463908900a289a3cfd6f88c814a94768
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-10-25 08:46:21 +0600

    Fetch player data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header/Header";
+import Body from "./components/body/Body";
 
 function App() {
   const [coins, setCoins] = useState(0);
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch("/players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
 
   const addMoreCoins = () => setCoins((c) => c + 1000000);
 
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Header coins={coins} addMoreCoins={addMoreCoins} />
+      <Body players={players} />
     </div>
   );
 }
